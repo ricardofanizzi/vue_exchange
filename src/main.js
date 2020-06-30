1f1de005f32b98873de12a68a631bb5010b112fc
ricardofanizzi <ricardo.fanizzi@gmail.com>
2020-06-30 14:05:13 +0200

    New project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+import { dollarFilter, percentFilter } from "@/filters";
+
+import router from "@/router";
+
+import Chartkick from "vue-chartkick";
+import Chart from "chart.js";
+
+import { VueSpinners } from "@saeris/vue-spinners";
+
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
+
+Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart));
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex justify-center">
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assets-table v-if="!isLoading" :assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+import api from "@/api";
+
+export default {
+  name: "Home",
+  components: {
+    PxAssetsTable,
+  },
+  data() {
+    return {
+      isLoading: false,
+      assets: [],
+    };
+  },
+  async created() {
+    this.isLoading = true;
+
+    this.assets = await api.getAssets();
+    this.isLoading = false;
+  },
+};
+</script>
